2086511bfa3e3ef1f9d9633ca9dac007f162a0b8
sakchai.limtsooknirun <sakchai.limtsooknirun@scb.co.th>
2019-07-22 16:07:00 +0700

    connect db

--- src/main/java/com/hackaton/nearme/services/MerchantService.java
+++ src/main/java/com/hackaton/nearme/services/MerchantService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class MerchantService {
 
-    private MerchantRepository merchantRepository;
-
-    public MerchantService(MerchantRepository merchantRepository){ this.merchantRepository = merchantRepository; }
+//    private MerchantRepository merchantRepository;
+//
+//    public MerchantService(MerchantRepository merchantRepository){ this.merchantRepository = merchantRepository; }
 }
