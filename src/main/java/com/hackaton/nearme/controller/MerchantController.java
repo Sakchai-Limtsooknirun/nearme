29eebf7b8749582cf526ff78df8e61cf0147ca2e
supitcha.srisirikulwattana <supitcha.srisirikulwattana@scb.co.th>
2019-07-22 15:40:57 +0700

    update merchant

--- src/main/java/com/hackaton/nearme/controller/MerchantController.java
+++ src/main/java/com/hackaton/nearme/controller/MerchantController.java
@@ -1,9 +1,13 @@
 package com.hackaton.nearme.controller;
 
+import com.hackaton.nearme.services.MerchantService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path="/merchant")
 public class MerchantController {
+    private MerchantService merchantService;
+
+    public MerchantController(MerchantService merchantService){ this.merchantService = merchantService; }
 }
--- src/main/java/com/hackaton/nearme/services/MerchantService.java
+++ src/main/java/com/hackaton/nearme/services/MerchantService.java
@@ -1,7 +1,12 @@
 package com.hackaton.nearme.services;
 
+import com.hackaton.nearme.repositories.MerchantRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MerchantService {
+
+    private MerchantRepository merchantRepository;
+
+    public MerchantService(MerchantRepository merchantRepository){ this.merchantRepository = merchantRepository; }
 }
